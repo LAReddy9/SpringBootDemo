32d63987e4f85746f3498b39a38d9f26aca36c7d
LAReddy <lareddy.9999@gmail.com>
2023-01-22 16:29:28 +0530

    test

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "This is from test api";
+        return "This is from get test api";
+    }
+
+    @PostMapping("/test")
+    public String testPost() {
+        return "This is from post test api";
     }
 }
